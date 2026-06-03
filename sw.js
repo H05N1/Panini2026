cd70dd00ea22ce0628848a3153c0b3b4fe125f2d
H05N1 <hosni.zahri@gmail.com>
2026-06-03 12:16:43 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE="panini-wc2026-v2";
+const ASSETS=["./","./index.html","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>{try{c.put(e.request,copy)}catch(_){}});
+      return resp;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
