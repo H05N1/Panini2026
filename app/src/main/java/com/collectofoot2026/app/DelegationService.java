dae324f3b650e6eed98e342e8d60cfe222fb7d72
H05N1 <elias.bazarr1@gmail.com>
2026-06-07 19:25:43 +0200

    v4.6 profil extra stickers

---
+++ app/src/main/java/com/collectofoot2026/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.collectofoot2026.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
